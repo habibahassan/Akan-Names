dd47d1d58b6e035c3efb9109599235b1370f7491
habibahassan <halimaadan92@gmail.com>
2019-12-01 16:56:18 +0300

    Adding functions to js script

--- js/scrips.js
+++ js/scrips.js
@@ -15,3 +15,19 @@ function getInput(){
     }else if (month == ""){
         alert("Input the correct month");
         return false;
+    }else if(dayOfMonth == ""){
+        alert("input the correct date");
+        return false;
+    }
+}
+
+function calculateDay(){
+    getInput();
+    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+    console.log(dayOfWeek); //Test the calculateDay function
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0) {
+        dayOfWeek = dayOfWeek * -1;
+    }
+    else if (dayOfWeek > 0) {
+        return dayOfWeek;
