7d2ade91f18db6009b2b7d9ee829550f9cbec920
habibahassan <halimaadan92@gmail.com>
2019-12-02 11:04:18 +0300

    update script.js

--- js/scrips.js
+++ js/scrips.js
@@ -1,33 +1,58 @@
-var century, year, month, dayOfMonth, dayOfWeek, day;
-function getInput(){
-    century = parseInt(document.getElementById("century").value);
-    year = parseInt(document.getElementById("year").value);
-    month = parseInt(document.getElementById("month").value);
-    dayOfMonth = parseInt(document.getElementById("monthday").value);
 
-
-    if(century == ""){
-        alert("Input the correct gender");
-        return false;
-    }else if (year == ""){
-        alert("Input the correct year");
-        return false;
-    }else if (month == ""){
-        alert("Input the correct month");
-        return false;
-    }else if(dayOfMonth == ""){
-        alert("input the correct date");
-        return false;
+  function validatemydate() {
+    var date = document.getElementsByName("date").value;
+   
+    if ((date <= 0) || (date > 31)) {
+      
     }
-}
-
-function calculateDay(){
-    getInput();
-    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
-    console.log(dayOfWeek); //Test the calculateDay function
-    return (Math.floor(dayOfWeek));
-    if (dayOfWeek < 0) {
-        dayOfWeek = dayOfWeek * -1;
+  }
+  function validatemymonth() {
+    var month = document.getElementsByName("month").value;
+    
+    if ((month <= 0) || (month > 12)) {
+      
     }
-    else if (dayOfWeek > 0) {
-        return dayOfWeek;
+  }
+  function validatemycentuary() {
+    var centuary = document.getElementsByName("Centuary").value;
+    
+    if ((centuary <= 0) || (centuary > 99));
+  }
+  function validatemyyear() {
+    var year = document.getElementsByName("year").value;
+    
+    if ((year <= 0) || (year > 99));
+     
+  }
+  function getakanname() {
+    var date = document.getElementsByName('date').value;
+    var month = document.getElementsByName('month').value;
+    var year = document.getElementsByName("year").value;
+    var century = document.getElementsByName("Centuary").value;
+    var gender;
+    var detail = document.getElementsByName('gender');
+  
+    for (i = 0; i < ele.length; i++) {
+      if (detail[i].checked)
+        gender = detail[i].value;
+    }
+   
+    var Weekday= (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
+  
+    
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    if (gender == "male") {
+      var d = " " + male[Math.floor(Weekday)] + "   :: " + days[Math.floor(Weekday)];
+      document.getElementsByName("name").innerHTML = d;
+      
+    }
+    else {
+      var d = " " + female[Math.floor(Weekday)] + "    :: " + days[Math.floor(Weekday)];
+      document.getElementsByName("name").innerHTML = d;
+      
+    }
+  }
+  
+  
