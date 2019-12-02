b64cb55ab7783b2a914aebdb7e7a678c60009ff0
habibahassan <halimaadan92@gmail.com>
2019-12-02 17:57:49 +0300

    indenting

--- js/scrips.js
+++ js/scrips.js
@@ -6,26 +6,26 @@ function validaymyday() {
 }
 function validaymymonth() {
   var month = document.getElementById("month").value;
-  
+
   if ((month <= 0) || (month > 12)) {
-      alert("Error!!! please enter correct month");
+    alert("Error!!! please enter correct month");
   }
 }
 function validaymycentury() {
   var centuary = document.getElementById("century").value;
-  
-  if ((centuary <= 0) || (centuary > 99)){
 
-      alert("Error!!! please enter correct century");
+  if ((centuary <= 0) || (centuary > 99)) {
+
+    alert("Error!!! please enter correct century");
   }
 }
 function validaymyyear() {
   var year = document.getElementById("year").value;
-  
-  if ((year <= 0) || (year > 99)){
-      alert("Error!!! please enter correct Year");
+
+  if ((year <= 0) || (year > 99)) {
+    alert("Error!!! please enter correct Year");
   }
-   
+
 }
 function getakanname() {
   var day = document.getElementById('day').value;
@@ -39,22 +39,22 @@ function getakanname() {
     if (detail[i].checked)
       gender = detail[i].value;
   }
- 
-  var Weekday= (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
 
-  
+  var Weekday = (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7;
+
+
   var femaleArray = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
   var maleArray = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
   var daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   if (gender == "male") {
     var xy = "Your Akan Name is: " + maleArray[Math.floor(Weekday)] + "  and Day of the week is: " + daysArray[Math.floor(Weekday)];
     document.getElementById("outAkan").innerHTML = xy;
-    
+
   }
   else {
     var xy = "Your Akan Name is: " + femaleArray[Math.floor(Weekday)] + "  and Day of the week is: " + daysArray[Math.floor(Weekday)];
     document.getElementById("outAkan").innerHTML = xy;
-    
+
   }
- 
+
 }
